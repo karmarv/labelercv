a5e7d68e64ca3be7ce4a0195187955770b13839b
Rahul Vishwakarma <vishwakarma.rhl@gmail.com>
2024-02-23 18:58:35 -0600

    upgrade to cvat v2.11.0 fails

--- cvat_scripts/shutdown_cvat.bash
+++ cvat_scripts/shutdown_cvat.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CVAT_VERSION="v2.9.2"
-export CVAT_HOST="$(hostname).local"
+export CVAT_VERSION="v2.11.0"
+export CVAT_HOST="$(hostname)"
 cd ../cvat
 # Shutdown docker containers
 docker compose -f docker-compose.local.yml -f components/serverless/docker-compose.serverless.yml down
@@ -13,6 +13,6 @@ else
     if [[ "$1" == delete ]] ; then
         echo "Delete the container and volumens" 
         docker compose -f docker-compose.local.yml -f components/serverless/docker-compose.serverless.yml  down --rmi all --volumes --remove-orphans
-        rm -rf ~/dev/cvat_data/*
+        rm -rf /mnt/c/dev/cvat_data/*
     fi
 fi
--- cvat_scripts/startup_cvat.bash
+++ cvat_scripts/startup_cvat.bash
@@ -1,5 +1,5 @@
-export CVAT_VERSION="v2.9.2"
-export CVAT_HOST="$(hostname).local"
+export CVAT_VERSION="v2.11.0"
+export CVAT_HOST="$(hostname)"
 cd ../cvat
 # Startup docker containers
 docker compose -f docker-compose.local.yml -f components/serverless/docker-compose.serverless.yml up -d
